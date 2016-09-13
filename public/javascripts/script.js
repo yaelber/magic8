719dc1c2b7e64dd52fcb12f243282887ba50eed7
Yael Bercow <yaelbercow@gmail.com>
2016-09-13 13:26:03 -0400

    second commit-updated files

---
+++ public/javascripts/script.js
@@ -0,0 +1,29 @@
+//LOGIC:
+//1. Promise should handle capturing question or alert user to enter text
+//2. Once that's done a random answer should be selected and presented
+
+//move the new promise to before the if
+
+
+$("#form").submit(function(event){
+  event.preventDefault();
+  var questionText = $("input[name=question]").val();
+  var magic8Ball = new Promise(function(resolve,reject) {
+    if (questionText.length != 0) {
+      setTimeout(function() {
+        resolve(answer);}, 3000);
+    } else {
+        reject(alert("Please enter a question in the input box!"))
+    }
+  });
+});
+
+magic8Ball.then(function(questionText){
+  var answerArray = ["You will live forever", "You're making the right decision","SO AWSOME!", "Nahhhh Dude"];
+  var randomAnswer = answerArray[Math.floor(Math.random() * answerArray.length)];
+  $("#answer").innerHTML = randomAnswer;
+});
+
+
+
+
