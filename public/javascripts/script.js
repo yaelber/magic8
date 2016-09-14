caf12e868781dd5477e39fab2a7c9368ab2e479a
Yael Bercow <yaelbercow@gmail.com>
2016-09-14 13:40:36 -0400

    latest files scrip and index.ejs

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,11 +3,12 @@
 //2. Once that's done a random answer should be selected and presented
 
 //move the new promise to before the if
+console.log("Hi");
 
 
-$("#form").submit(function(event){
-  event.preventDefault();
+$("#magic8").submit(function(event){
   var questionText = $("input[name=question]").val();
+  event.preventDefault();
   var magic8Ball = new Promise(function(resolve,reject) {
     if (questionText.length != 0) {
       setTimeout(function() {
